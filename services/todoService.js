31175daa20fda2b7d9fbb9e34e41c9adba92c4d0
RuslanName <ruslangeroy@gmail.com>
2025-04-17 06:56:03 +0300

    Update todoService.js

--- services/todoService.js
+++ services/todoService.js
@@ -1,46 +1,48 @@
-const { Todo } = require('../models/todo');
+const Todo = require('../models/todo');
 
-const getTodos = async() => {
-    try {
-        return await Todo.findAll();
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
-}
-
-const addTodo = async(text) => {
-    try {
-        return await Todo.create({ text });
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
-}
-
-const updateTodo = async(id, text) => {
-    try {
-        const [updated] = await Todo.update({ text }, { where: { id } });
-        if (updated) {
-            return await Todo.findByPk(id);
+const todoService = {
+    getTodos: async () => {
+        try {
+            return await Todo.findAll();
+        } catch (err) {
+            console.error(err);
+            throw err;
         }
-        throw new Error('Todo not found');
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
-}
+    },
 
-const deleteTodo = async(id) => {
-    try {
-        const deleted = await Todo.destroy({ where: { id } });
-        if (!deleted) {
+    addTodo: async (text) => {
+        try {
+            return await Todo.create({text});
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
+    },
+
+    updateTodo: async (id, text) => {
+        try {
+            const [updated] = await Todo.update({text}, {where: {id}});
+            if (updated) {
+                return await Todo.findByPk(id);
+            }
             throw new Error('Todo not found');
+        } catch (err) {
+            console.error(err);
+            throw err;
         }
-    } catch (err) {
-        console.error(err);
-        throw err;
+    },
+
+    deleteTodo: async (id) => {
+        try {
+            const deleted = await Todo.destroy({where: {id}});
+            if (!deleted) {
+                throw new Error('Todo not found');
+            }
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
     }
 }
 
-module.exports = { getTodos, addTodo, updateTodo, deleteTodo };
+module.exports = todoService;
